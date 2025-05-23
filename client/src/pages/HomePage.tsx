29ec4edfd657f3b00b61c5a756545e814baca641
Vishwa <vishwashankar1304@gmail.com>
2025-05-23 12:49:57 +0530

    git image added

--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -77,7 +77,7 @@ const HomePage = () => {
           </div>
           <div className="hidden md:block">
             <img 
-              src="https://images.unsplash.com/photo-1556155092-490a1ba16284?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2670&q=80"
+              src='/lovable-uploads/images/products/siva traders.jpg'
               alt="Electricals Showcase"
               className="rounded-lg shadow-lg"
             />
